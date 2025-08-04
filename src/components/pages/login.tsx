2be67c9a232ba2fe58833ca47878fba15247d5a0
lastforever457 <lastforever457@gmail.com>
2025-08-04 11:48:11 +0500

    fix env

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import logo from "../../../public/logo.png";
+import logo from "../../../public/img-bg.png";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 import { toast } from "sonner";
@@ -48,9 +48,9 @@ export default function Login() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-white to-purple-100 flex items-center justify-center p-4">
       <Card className="w-full max-w-md shadow-xl border-0 bg-white/95 backdrop-blur-sm">
-        <CardHeader className="text-center space-y-4 pb-6">
-          <div className="mx-auto w-32 bg-stone-300 flex items-center justify-center">
-            <Image src={logo} alt={"logo"} priority />
+        <CardHeader className="text-center space-y-4">
+          <div className="mx-auto w-32 flex items-center justify-center">
+            <Image src={logo} alt={"logo"} priority quality={100} />
           </div>
         </CardHeader>
 
