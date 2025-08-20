55974a504d4f1bdefddddcee63d9416459e2debd
dilshod505 <vohidjonov505@gmail.com>
2025-08-20 13:03:19 +0500

    fix: landing page

--- src/app/[locale]/(user)/layout.tsx
+++ src/app/[locale]/(user)/layout.tsx
@@ -2,21 +2,29 @@ import React from "react";
 import FullScreen from "@/components/widgets/full-screen";
 import ThemeSwitcher from "@/components/widgets/theme-switcher";
 import ChangeLanguage from "@/components/widgets/change-language";
-import { Luxurious_Roman } from "next/dist/compiled/@next/font/dist/google";
+import logo from "../../../../public/logo-full.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <header className="cont flex h-16 shrink-0 justify-between items-center gap-2 border-b px-4">
-        <div className="flex justify-center items-center gap-3">
-          <h1 className={`text-2xl font-semibold py-5`}>AIFU</h1>
-        </div>
-        <div className="flex justify-center items-center gap-2">
-          <FullScreen />
-          <ThemeSwitcher />
-          <ChangeLanguage />
-        </div>
-      </header>
+      <div className="bg-[#213148]">
+        <header className="cont flex h-16 shrink-0 justify-between items-center gap-2 px-4 ">
+          <div className="flex justify-center items-center gap-3">
+            <Link href="/">
+              <h1 className={`text-2xl font-semibold py-5`}>
+                <Image src={logo} alt={""} />
+              </h1>
+            </Link>
+          </div>
+          <div className="flex justify-center items-center gap-2 text-white">
+            <FullScreen />
+            <ThemeSwitcher />
+            <ChangeLanguage />
+          </div>
+        </header>
+      </div>
       {children}
     </>
   );
