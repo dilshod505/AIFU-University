4e199764c3cf9e983672e5dd4fb489c3d8250e7d
lastforever457 <lastforever457@gmail.com>
2025-08-28 02:00:45 -0700

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
 import createMiddleware from "next-intl/middleware";
+import { NextRequest } from "next/server";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(req: NextRequest) {
+  const res = intlMiddleware(req);
+  res.headers.set("x-custom-header", "my-custom-value");
+
+  res.headers.set("Cache-Control", "max-age=3600");
+
+  return res;
+}
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
